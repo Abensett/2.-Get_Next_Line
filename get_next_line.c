48925e6748923e8769c98a8313734130935525c1
Abensett <85625233+Abensett@users.noreply.github.com>
2021-09-11 20:29:16 +0200

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -11,32 +11,92 @@
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#define BUF_SIZE 4096
 
-
-
-int main(void)
+char	*ft_strdup(const char *src)
 {
+	char			*dup;
+	unsigned int	lensrc;
 
-	int fd;
-	int txt;
-	char buf[BUF_SIZE + 1];
-	
-	fd = open("extrait.txt",O_RDONLY); /* RETURN  AN INT THAT IS LINKED TO A FILE*/
-	if (fd == -1)
-	{
-		printf(("open() error"));
-		return(-1);
-	}
-	txt = read(fd, buf, BUF_SIZE);	/* RETURN : -1 = ERROR     INT = NB OF OCTETS*/
-	if (txt == -1)
-		return  (-1);
-	buf[txt] = '\0';
-	printf("%s",buf);
-
-	return(0);
+	lensrc = 0;
+	while (src[lensrc])
+		lensrc++;
+	dup = malloc(sizeof(char) * (lensrc + 1));
+	if (!dup)
+		return (NULL);
+	dup[lensrc] = '\0';
+	while (lensrc-- != 0)
+		dup[lensrc] = src[lensrc];
+	return (dup);
 }
 
+char	*ft_strnew(size_t size)
+{
+	char	*s;
+
+	s = malloc((size + 1) * sizeof(char));
+	if (s == NULL)
+		return (NULL);
+	ft_memset(s, 0, size);
+	return (s);
+}
+
+void	ft_memdel(void **ptr)
+{
+	if (*ptr && ptr)
+	{
+		ft_memset(*ptr, 0, ft_strlen(*ptr));
+		free(*ptr);
+		*ptr = NULL;
+	}
+}
+
+char	*get_line_printed(char **line, ssize_t len)
+{
+	char	*line_to_print;
+	int		i;
+	char	*tmp;
+
+	i = 0;
+	if (len == 0)
+		line_to_print = ft_strdup(*line);
+	else
+		line_to_print = ft_substr(*line, 0, (ft_strchr(*line, '\n') - *line));
+	while (line_to_print[i])
+	{
+		write(1, &line_to_print[i], 1);
+		i++;
+	}
+	if (len != 0)
+		write(1, "\n", 1);
+	if (len > 0)
+		tmp = ft_strdup((*line + (ft_strlen(line_to_print) + 1)));
+	else
+		tmp = ft_strdup((*line + ft_strlen(line_to_print)));
+	ft_memdel((void **)line);
+	*line = tmp;
+	return (line_to_print);
+}
+
+char	*get_next_line(int fd)
+{
+	ssize_t		len;	
+	char		buff[BUFFER_SIZE + (len = 1)];
+	static char	*line;
+	char		*tmp;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	if (line == NULL)
+		line = ft_strnew(0);
+	while (!ft_strchr(line, '\n') && len > 0)
+	{
+		len = read(fd, buff, BUFFER_SIZE);
+		buff[len] = '\0';
+		tmp = ft_strjoin(line, buff);
+		ft_memdel((void **)&line);
+		line = tmp;
+	}
+	if (len < 0)
+		return (NULL);
+	return (get_line_printed(&line, len));
+}
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -10,33 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strnstr(const char *big, const char *little, size_t n)
-{
-	int		i;
-	int		d;
-	int		f;
+#include "get_next_line.h"
 
-	if (little[0] == '\0')
-		return ((char *) big);
-	i = 0;
-	while (big[i] && n != 0)
-	{
-		if (n < ft_strlen(little))
-			return (NULL);
-		d = 0;
-		f = i;
-		while (big[f] == little[d] && big[f] && little[d])
-		{
-			d++;
-			f++;
-		}
-		if (little[d] == '\0')
-			return ((char *) big + i);
-		n--;
-		i++;
-	}
-	return (NULL);
-}
 char	*ft_strchr(const char *s, int c)
 {
 	int		i;
@@ -53,4 +28,78 @@ char	*ft_strchr(const char *s, int c)
 	if (s[i] == (char)c)
 		return ((char *)&s[i]);
 	return (0);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*head;
+	char	*str;
+	int		len;
+
+	len = 0;
+	if (!s1 && !s2)
+		return (NULL);
+	if (!s1 && s2)
+		len = ft_strlen(s2) + 1;
+	else if (s1 && s2)
+		len = ft_strlen(s1) + ft_strlen(s2) + 1;
+	str = malloc(sizeof(*str) * (len));
+	head = str;
+	if (!str)
+		return (NULL);
+	if (s1)
+		while (*s1)
+			*str++ = *s1++;
+	if (s2)
+		while (*s2)
+			*str++ = *s2++;
+	*str = '\0';
+	return (head);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (*(s++))
+		i++;
+	return (i);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*str;
+	size_t	i;
+
+	if (s == NULL)
+		return (NULL);
+	i = 0;
+	if (ft_strlen(s) < start)
+		return (ft_strdup(""));
+	if (len > ft_strlen(s) - start)
+		len = ft_strlen(s) - start;
+	str = malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (NULL);
+	while (i < len)
+		str[i++] = s[start++];
+	str[i] = '\0';
+	return (str);
+}
+
+void	*ft_memset(void *s, int c, size_t n)
+{
+	char	*a;
+
+	if (s == NULL)
+		return (NULL);
+	a = s;
+	while (n)
+	{
+		*a = c;
+		a++;
+		n--;
+	}
+	return (s);
 }
